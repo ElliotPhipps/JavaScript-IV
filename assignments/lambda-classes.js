b17b4fe5e389a71727ef591f695b9c32d2fe8926
Elliot Phipps <phipps.elliot@gmail.com>
2019-10-17 18:59:33 -0400

    Finished

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -29,10 +29,10 @@ class Instructor extends Person{
     }
     /* has the following methods: `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.,`grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
     */
-    demo(subject){
-        return `'Today we are learning about ${subject}.`
+    demo(subject = 'JavaScript'){
+        return `Today we are learning about ${subject}.`
     }
-    grade(student, subject){
+    grade(student = Elliot, subject = 'JavaScript'){
         return `${student.name} receives a perfect score on ${subject}.`
     }
 }
@@ -49,15 +49,13 @@ class Student extends Person{
     }
     //methods: `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one. `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`, `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
     listsSubjects(){
-        this.favSubjects.forEach(element => {
-            return console.log(element);
-        });
+        this.favSubjects.forEach(element => {console.log(element)})
     }
-    PRAssignment(subject){
-        return `${student.name} has submitted a PR for ${subject}.`
+    PRAssignment(subject = 'JavaScript'){
+        return `${this.name} has submitted a PR for ${subject}.`
     }
-    sprintChallenge(subject){
-        return `${student.name} has begun springt challenge on ${subject}.`
+    sprintChallenge(subject = 'JavaScript'){
+        return `${this.name} has begun sprint challenge on ${subject}.`
     }
 }
 
@@ -68,15 +66,116 @@ class ProjectManager extends Instructor{
         super(abilities);
         //ProjectManagers have the following unique props: `gradClassName`: i.e. CS1, `favInstructor`: i.e. Sean
         this.gradClassName = abilities.gradClassName,
-        this.faveInstructor = abilities.faveInstructor
+        this.favInstructor = abilities.favInstructor
     }
     //methods: `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!`​,`debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
-    standUp(name, channel){
-        return `${name} announces to ${channel}, @channel standy times!`
+    standUp(channel = '#Web25_Tim'){
+        return `${this.name} announces to ${channel}, @channel standy times!`
     }
-    debugsCode(name, subject){
-        return `${name} debugs ${student.name}'s code on ${subject}.`
+    debugsCode(Student = Elliot, subject = "JavaScript"){
+        return `${this.name} debugs ${Student.name}'s code on ${subject}.`
     }
 }
 
+//instructor objects
 
+const Dan  = new Instructor({
+    name: 'Dan',
+    location: 'USA',
+    age: 32,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Let's code!`
+});
+
+const Josh = new Instructor({
+    name: 'Josh',
+    location: 'USA',
+    age: 30,
+    favLanguage: 'JavaScript',
+    specialty: 'Full-stack',
+    catchPhrase: `Let's have fun!`
+});
+
+const Brit = new Instructor({
+    name: 'Brit',
+    location: 'Canada',
+    age: 29,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `It's getting cold out here, eh?`
+});
+
+//student objects
+
+const Elliot = new Student({
+    name: 'Elliot',
+    location: 'Michigan',
+    age: 24,
+    previousBackground: 'Worked as a Press Op',
+    className: 'Web25',
+    favSubjects: ['Python', 'Javascript', 'Ruby']
+});
+
+const Chance = new Student({
+    name: 'Chance',
+    location: 'California',
+    age: 50,
+    previousBackground: 'DVD writing',
+    className: 'Web25',
+    favSubjects: ['Python', 'Javascript', 'Ruby', 'Dreamweaver']
+});
+
+const Cameron = new Student({
+    name: 'Cameron',
+    location: 'The land that was fought over',
+    age: 28,
+    previousBackground: 'Lenin',
+    className: 'Web25',
+    favSubjects: ['HTML', 'CSS', 'Javascript']
+});
+
+//project manager objects
+
+const Tim = new ProjectManager({
+    name: 'Tim',
+    location: 'Oregon',
+    age: 28,
+    specialty: 'JavaScript',
+    favLanguage: 'Java',
+    catchPhrase: 'Cool, cool!',
+    gradClassName: 'Web25',
+    favInstructor: 'Josh'
+});
+
+const Tommy = new ProjectManager({
+    name: 'Tommy', 
+    location: '?',
+    age: 'guess' ,
+    specialty: 'Node.Js', 
+    favLanguage: 'C#', 
+    catchPhrase: `What's sleep?`, 
+    gradClassName: 'Web21',
+    favInstructor: 'Luis'
+});
+
+const Bartholemew = new ProjectManager({
+    name: 'Bartholomew',
+    location: 'Over the rainbow',
+    age: 25,
+    specialty: 'Java', 
+    favLanguage: 'C', 
+    catchPhrase: `Who's HTML?`,
+    gradClassName: 'Web22',
+    favInstructor: 'Brit'
+});
+
+//console.logs
+
+console.log(Brit.demo());
+console.log(Brit.grade());
+console.log(Elliot.listsSubjects());
+console.log(Elliot.PRAssignment());
+console.log(Elliot.sprintChallenge());
+console.log(Tim.standUp());
+console.log(Tim.debugsCode());
